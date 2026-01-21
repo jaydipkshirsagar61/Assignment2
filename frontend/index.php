5294e262289bbfe972eea6e38d6f5bb86c01a2a9
jaydip <jaydipksagar14@gmail.com>
2026-01-21 22:15:25 +0530

    improved structure of entire frontend by adding different folder as well as also added logic through which user routed to login page or main dashboard

--- frontend/index.php
+++ frontend/index.php
@@ -4,22 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Search Project</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/styles/style.css">
 </head>
 <body>
-
-<div class="app-wrapper">   
-<?php
-require_once __DIR__ . '/component/header.php';
-require_once __DIR__ . '/component/search.php';
-require_once __DIR__ . '/component/history.php';
-?>
-</div>
-<div class="auth-wrapper flex center">
-    <div id="auth-container" class="flex flex-col center">
-        <?php include __DIR__ . '/login.php'; ?>
+   <?php $login=true ?>
+<?php if ($login==true): ?>
+    <div class="app-wrapper">
+        <?php
+        require_once __DIR__ . '/component/header.php';
+        require_once __DIR__ . '/component/search.php';
+        require_once __DIR__ . '/component/history.php';
+        ?>
     </div>
-</div>
+<?php else: ?>
+    <div class="auth-wrapper flex center">
+        <div id="auth-container" class="flex flex-col center">
+            <?php include __DIR__ . '/pages/login.php'; ?>
+        </div>
+    </div>
+<?php endif; ?>
 
 
 </body>
